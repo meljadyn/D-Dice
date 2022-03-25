b7c701f10b598414e52dfe30ffdc87e31ab4d2f2
Jadyn <33948021+meljadyn@users.noreply.github.com>
2022-03-25 14:35:14 -0400

    Delete week01 directory

--- week01/hello/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h> // required for get_string
-
-int main(void)
-{
-    string name = get_string("What is your name? "); // gets name
-    printf("hello, %s\n", name); // prints "hello, [name]"
-}
